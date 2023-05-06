2aedc5230b0dd8272e22c6080b9a8ddabca1d4ea
Nursaule <admin@MacBook-Pro.homenet.telecomitalia.it>
2023-05-06 20:53:03 +0200

    Updates

---
+++ precache-manifest.db8a1d37d81243923a6e909a863912e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce36c0625a1e36e41952",
+    "url": "/robo/static/css/main.93196720.chunk.css"
+  },
+  {
+    "revision": "ce36c0625a1e36e41952",
+    "url": "/robo/static/js/main.ce36c062.chunk.js"
+  },
+  {
+    "revision": "0f4966a23fb3dc5dd026",
+    "url": "/robo/static/css/1.d9fba003.chunk.css"
+  },
+  {
+    "revision": "0f4966a23fb3dc5dd026",
+    "url": "/robo/static/js/1.0f4966a2.chunk.js"
+  },
+  {
+    "revision": "e0336b123eb1f66a9ec9",
+    "url": "/robo/static/js/runtime~main.e0336b12.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robo/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "fc245e629dd108a61403ab28416c0960",
+    "url": "/robo/index.html"
+  }
+];
